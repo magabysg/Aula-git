29d17a4ce9cadc1942e8b907351bd4560e1f5b42
MARIA GABRIELA CRUZ DA SILVA <maria.silva1577@senaisp.edu.br>
2026-03-18 15:07:27 -0300

    feat(Ex01): cadastro de aluno

---
+++ src/Exercicio01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    void main(){
+
+        Scanner sc = new Scanner(System.in);
+
+        IO.println("Para realizae seu cadastro, digite suas informações abaixo: \n");
+        IO.println("Nome do aluno(a):");
+        String nome = sc.nextLine();
+
+        IO.println("Curso que o Aluno(a) está matriculado:");
+        String curso = sc.nextLine();
+
+        IO.println("Informe a idade do Aluno(a):");
+        int idade = sc.nextInt();
+
+        IO.println("         Verifique sua informações do Aluno(a)         \n");
+        IO.println("Nome: " + nome);
+        IO.println("Curso: " + curso);
+        IO.println("Idade: " + idade);
+
+    }
+}
