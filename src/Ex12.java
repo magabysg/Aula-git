437dc317b5aeff5810615983f571d60480704ac2
magabysg <gabysthrd@gmail.com>
2026-04-08 13:48:01 -0300

    feat(Ex13): uso de internet

--- src/Ex12.java
+++ src/Ex12.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex12 {
+
+    void main(){
+
+        Scanner sc = new Scanner(System.in);
+
+        IO.println("\nInforme o total de aulas do seu curso e o total de aulas que o aluno(a) já assistiu:");
+        double curso = sc.nextDouble();
+        IO.println("Total de aulas: " + curso);
+        double aulas = sc.nextDouble();
+        IO.println("Total de aulas assistidas: " + aulas);
+
+        if (aulas == curso) {
+            IO.println("\nVocê concluiu o seu curso! 100% do curso completo.");
+
+        } else if (aulas < curso) {
+            double percentual = (aulas / curso * 100);
+            IO.println("\nVocê concluiu " + percentual + "% do curso.");
+
+            if (percentual < 50) {
+                IO.println("\nVocê ainda está no começo do curso.");
+
+            } else if (percentual >= 50 && percentual < 100) {
+                IO.println("\nVocê está avançando bem!");
+
+            }
+        }
+    }
 }
--- src/Ex13.java
+++ src/Ex13.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex13 {
+
+    void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        double limite = 20000;
+        IO.println("\nSeu limite de internet: 20GB.\n");
+        IO.println("Quanto você já usou do seu pacote de internet?");
+        double usado = sc.nextDouble();
+
+        double you = usado / limite * 100;
+
+        if (you > 100) {
+            IO.println("Limite excedido! " + you + "%");
+        } else if (you >= 80 & you <= 100) {
+            IO.println("Atenção! Você está próximo do limite! " + you + "%");
+        } else if (you < 80) {
+            IO.println("Uso dentro do limite. " + you + "%");
+        }
+    }
 }
